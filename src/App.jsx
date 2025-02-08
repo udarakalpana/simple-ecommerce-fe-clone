06c216d05cb18f171893c22646f5cd3ff1db6287
kalpana udara <kalpanaudara058@gmail.com>
2025-02-08 20:47:01 +0530

    Config axios http client

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import Routers from "./routers/Routers.jsx";
+import axios from "axios";
 
 const App = () => {
+    axios.defaults.baseURL = 'https://api.codinggura.com/'
+    axios.defaults.headers.get['Content-Type'] = 'application/json'
+    axios.defaults.headers.get['Accept'] = 'application/json'
+
+    axios.interceptors.request.use(function (config) {
+        config.headers.Authorization = 'Bearer 871|bysAIPhIVNaMyFHYZguvZT1FsZsLJg8nqyORYz3Aae4b86d0'
+        return config;
+    })
 
   return (
       <>
