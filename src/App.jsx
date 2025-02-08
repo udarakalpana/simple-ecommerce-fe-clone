49f1fdf7681741f2e248e6db1ad179bfbfb43042
kalpana udara <kalpanaudara058@gmail.com>
2025-02-08 20:47:01 +0530

    Config axios http client

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 
 const App = () => {
     axios.defaults.baseURL = 'https://api.codinggura.com/'
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'https://lms.codinggura.com'
     axios.defaults.headers.get['Content-Type'] = 'application/json'
     axios.defaults.headers.get['Accept'] = 'application/json'
+    axios.defaults.withCredentials = true;
 
     axios.interceptors.request.use(function (config) {
         config.headers.Authorization = 'Bearer 871|bysAIPhIVNaMyFHYZguvZT1FsZsLJg8nqyORYz3Aae4b86d0'
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,18 @@
 import ProductDetailsCard from "./ProductDetailsCard.jsx";
+import {useEffect} from "react";
+import axios from "axios";
 
 const Product = () => {
+
+    useEffect(() => {
+        fetchProductDetails()
+    }, []);
+
+    const fetchProductDetails = async () => {
+       const response = await axios.get('api/get-all-products')
+        console.log('sfasd')
+    }
+
     return (
         <div className='grid grid-cols-3 m-4'>
             <ProductDetailsCard/>
